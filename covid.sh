75b5d4f42efbdf2a200a882a13789906279de26d
JAN-UWE REICHERT <janreichert@users.noreply.github.com>
2021-11-17 15:56:06 +0100

    Update covid.sh
    Added extract from ECDC to get data earlier, WHO updates only in the evening Europe time. Also revamped the code. It is now easy to add countries or states that data should be extracted for.
    Added two options: 
    -s (to skip updated from the web, mainly for script testing)
    -l n (to run with a sleep of n seconds, default 2h, until an update is detected)

--- covid.sh
+++ covid.sh
@@ -1,53 +1,151 @@
 #!/bin/bash
 WORKDIR="/Users/jeichert/tmp"
 
-echo "Country;24h;7days" > $WORKDIR/out
+ECDC="https://opendata.ecdc.europa.eu/covid19/nationalcasedeath_eueea_daily_ei/csv/data.csv"
+NYTIMES="https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
+WHO="https://covid19.who.int/WHO-COVID-19-global-table-data.csv"
 
-# Processing Alaska from NYTimes
-printf "\n****************\nProcessig Alaska\n****************\n\n"
-curl -s "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv" | grep Alaska | tail -n 8 > $WORKDIR/data.csv
-sort -r $WORKDIR/data.csv | awk -F "\"*,\"*" '{print $4}' > $WORKDIR/calc.tmp
-NEW=`head -n 1 $WORKDIR/calc.tmp`
-OLD24H=`head -n 2 $WORKDIR/calc.tmp | tail +2`
-OLD7D=`tail -n 1 $WORKDIR/calc.tmp`
-DIFF7D=`expr $NEW - $OLD7D`
-echo "Alaska;`expr $NEW - $OLD24H`;`expr $DIFF7D / 7`" >> $WORKDIR/out
-rm $WORKDIR/calc.tmp
 
-printf "\n****************\nProcessig WHO\n****************\n\n"
+SKIP="N"
+LOOP="N"
 
-# Fetch WHO Data
-curl -s "https://covid19.who.int/WHO-COVID-19-global-table-data.csv" > $WORKDIR/data.csv
+usage() {
+    echo $(basename "$0") [-s] [-l n]
+    echo "-s skip dataset fetch (for testing purposes)"
+    echo "-l loop (experimental, sleep for n seconds, default 7200=2hours)"
+    echo
+    echo "current datasets:"
+    echo "   NYTIMES: $NYTIMES"
+    echo "   ECDC: $ECDC"
+    echo "   WHO: $WHO"
+    exit 1
+}
 
-# replace Commas by Semicolons
-sed "s/\,/\;/g" $WORKDIR/data.csv > $WORKDIR/data_tmp.csv
-rm $WORKDIR/data.csv
+while (($#)); do 
+    case $1 in
+        -s)
+            SKIP="Y"
+        ;;
+        -l)
+            LOOP="Y"
+            if [[ $2 = "" ]]; then
+                SLEEP=7200
+            else
+                SLEEP=$2
+            fi
+            shift
+        ;;
+        *)
+            usage;;
+    esac
+    shift
+done
 
-# Extract data we are interested in
-egrep -e 'United States of A' \
-      -e 'Germany'            \
-      -e 'France'             \
-      -e 'The United Kingdom' \
-      -e 'Italy'              \
-      -e 'Spain'              \
-      $WORKDIR/data_tmp.csv | awk -F "\"*;\"*" '{print $1 ";" $7 ";" $5/7}' >> $WORKDIR/out
+PREVIOUS="N"
+if [ -f $WORKDIR/out.csv.previous ]; then
+    PREVIOUS="Y"
+fi
 
-sed "s/\./\,/g" $WORKDIR/out > $WORKDIR/out.csv
+UPDATED="N"
 
-# Cleanup
-rm $WORKDIR/out
-rm $WORKDIR/data_tmp.csv
+while true; do
+    clear
+    echo "Country;24h;7days" > $WORKDIR/out.csv
+
+    # Processing NYTimes
+    if [[ $SKIP = "N" ]]; then
+        printf "\nFetching NYTIMES Dataset\n"
+        curl -s $NYTIMES > $WORKDIR/nytimes.csv 
+    fi
+    for STATE in "Alaska" "Washington"; do
+        printf "Processing "
+        grep $STATE $WORKDIR/nytimes.csv | tail -n 8 | sort -r | awk -F "\"*,\"*" '{print $4}' > $WORKDIR/$STATE.csv
+        NEW=`head -n 1 $WORKDIR/$STATE.csv`
+        OLD24H=`head -n 2 $WORKDIR/$STATE.csv | tail +2`
+        OLD7D=`tail -n 1 $WORKDIR/$STATE.csv`
+        DIFF7D=`expr $NEW - $OLD7D`
+        OUTPUT="$STATE;`expr $NEW - $OLD24H`;`expr $DIFF7D / 7`"
+        echo $OUTPUT
+
+        if [[ $PREVIOUS = "Y" ]]; then
+            if [[ $OUTPUT != `grep $STATE $WORKDIR/out.csv.previous` ]]; then
+                echo "   UPDATED"
+            fi 
+        fi
+        echo $OUTPUT >> $WORKDIR/out.csv
+        #rm $WORKDIR/$STATE.csv
+    done
+
+    # Processing ECDC
+    if [[ $SKIP = "N" ]]; then
+        printf "\nFetching ECDC Dataset\n"
+        curl -s $ECDC > $WORKDIR/ecdc.csv
+    fi
+    for COUNTRY in "Austria" "France" "Germany" "Italy" "Spain"; do
+        printf "Processing "
+        grep $COUNTRY $WORKDIR/ecdc.csv | head -n 7 | awk -F "\"*,\"*" '{print $5}' > $WORKDIR/$COUNTRY.csv
+        NEW=`head -n 1 $WORKDIR/$COUNTRY.csv`
+        TOTAL7D=0
+        for CASES in `cat $WORKDIR/$COUNTRY.csv`; do
+            TOTAL7D=`expr $TOTAL7D + $CASES`
+        done
+        OUTPUT="$COUNTRY;$NEW;`expr $TOTAL7D / 7`"
+        echo $OUTPUT
+
+        if [[ $PREVIOUS = "Y" ]]; then
+            if [[ $OUTPUT != `grep $COUNTRY $WORKDIR/out.csv.previous` ]]; then
+                echo "   UPDATED"
+            fi 
+        fi
+        echo $OUTPUT >> $WORKDIR/out.csv
+        #rm $WORKDIR/$COUNTRY.csv
+    done
+
+    # Processing WHO
+    if [[ $SKIP = "N" ]]; then
+        printf "\nFetching WHO Dataset\n"
+        curl -s $WHO > $WORKDIR/who.csv
+    fi
+    for COUNTRY in "The United Kingdom" "United States of America"; do
+        printf "Processing "
+        OUTPUT=`grep "$COUNTRY" $WORKDIR/who.csv | sed "s/\,/\;/g"    \
+            | awk -F "\"*;\"*" '{print $1 ";" $7 ";" $5/7}' \
+            | sed "s/\([^\.]*\).*/\1/"`
+        echo $OUTPUT
+
+        if [[ $PREVIOUS = "Y" ]]; then
+            if [[ $OUTPUT != `grep "$COUNTRY" $WORKDIR/out.csv.previous` ]]; then
+                echo "   UPDATED"
+            fi 
+        fi
+        echo $OUTPUT >> $WORKDIR/out.csv        
+    done
+
+    # create a save file to verify if there was an update
+    if [[ $PREVIOUS = "Y" ]]; then
+        DIFF=`diff $WORKDIR/out.csv $WORKDIR/out.csv.previous`
+        if [[ $DIFF != "" ]]; then
+            UPDATED="Y"
+        fi
+    else
+        cp $WORKDIR/out.csv $WORKDIR/out.csv.previous
+    fi
+    
+    if [[ $LOOP = "N" || $UPDATED = "Y" ]]; then
+        break
+    fi
+    sleep $SLEEP
+done
+
+printf "\n* Output"
+if [[ $UPDATED = "Y" ]]; then
+    rm $WORKDIR/out.csv.previous 2> /dev/null
+    printf " *** UPDATED ***"
+    cp $WORKDIR/out.csv $WORKDIR/out.csv.previous
+fi
+
+printf "\n\n"
 
 #Display the result
 cat $WORKDIR/out.csv
-
-# create a save file to verify if there was an update
-if [ -f $WORKDIR/out.csv.previous ]; then
-    DIFF=`diff $WORKDIR/out.csv $WORKDIR/out.csv.previous`
-    if [[ $DIFF != "" ]]; then
-        printf "\n** UPDATED **\n\n"
-    fi
-
-    rm $WORKDIR/out.csv.previous 2> /dev/null
-fi
-cp $WORKDIR/out.csv $WORKDIR/out.csv.previous
+printf "\n"
